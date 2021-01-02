b11d48a7231a9b600209102163186448ceb31bdf
Avishek Paul <57027258+av1paul@users.noreply.github.com>
2021-01-02 12:54:44 +0530

    Avishek Paul

---
+++ 01/hawt_sauce.py
@@ -0,0 +1,22 @@
+"""
+Avishek Paul
+B19CS038
+hawt_sauce
+"""
+class Solution:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        d = collections.defaultdict()
+        for piece in pieces:
+            d[piece[0]] = piece 
+        i = 0
+        while i < len(arr):
+            if arr[i] not in d:
+                return False
+            else:
+                piece_list = d[arr[i]]
+                for num in piece_list:
+                    if num == arr[i]:
+                        i += 1  
+                    else:
+                        return False
+        return True
