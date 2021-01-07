78bd3445b47a462d2a2aa0abc4ed0561b0a4aa1e
tanikant <76862936+tanikant@users.noreply.github.com>
2021-01-07 23:09:33 +0530

    Tanisha Kant

---
+++ 06/marmalide_bushes.cpp
@@ -0,0 +1,21 @@
+/*
+Name: Tanisha Kant
+Roll no: B19EC002
+*/
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int result =0 ,num=1, i =0;
+        while(k != 0)
+        {
+            if(i< arr.size() && arr[i] == num)
+                i++;
+            else
+                k--;
+            if(k==0)
+                result= num;
+            num++;
+        }
+        return result;
+    }
+};
