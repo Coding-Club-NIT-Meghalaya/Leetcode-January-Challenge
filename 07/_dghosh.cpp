a97687596f095948953ef92bc6027c37548c2564
depuchan <76862434+depuchan@users.noreply.github.com>
2021-01-08 22:06:16 +0530

    Debayan Ghosh

---
+++ 07/_dghosh.cpp
@@ -0,0 +1,20 @@
+/* Name: Debayan Ghosh
+   Roll No: B19ME002
+   LeetCode: _dghosh*/
+   class Solution {
+public:
+    inline int findKthPositive(vector<int>& arr, int k) {
+        int pos,i=1;
+        vector<int> nos;
+        for(int j=0;j<arr.size();++j){
+            while(i!=arr[j]){
+                nos.push_back(i);
+                ++i;
+            }
+            ++i;
+        }
+       for(auto it:nos) cout<<it<<" ";
+       if(k>nos.size()) return arr[arr.size()-1]+(k-nos.size());
+       else return nos[k-1];
+    }
+};
